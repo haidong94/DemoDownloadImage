1a8fe969d716c27add346edcccbc4889711350a3
haidong94 <haidong194@gmail.com>
2017-09-24 00:08:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dong/demodownloadimage/ImageDownloadAndSave.java
@@ -0,0 +1,87 @@
+package com.example.dong.demodownloadimage;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by DONG on 23-Sep-17.
+ */
+
+public class ImageDownloadAndSave extends AsyncTask<String, Void, Bitmap>
+{
+    @Override
+    protected Bitmap doInBackground(String... arg0)
+    {
+        downloadImagesToSdCard("","");
+        return null;
+    }
+
+    private void downloadImagesToSdCard(String downloadUrl,String imageName)
+    {
+        try
+        {
+            URL url = new URL("https://png.pngtree.com/thumb_back/fh260/back_pic/00/01/80/73560a545c6ae6b.jpg");
+                        /* making a directory in sdcard */
+            String sdCard= Environment.getExternalStorageDirectory().toString();
+            File myDir = new File(sdCard,"/sdcard/Images/test.jpg");
+
+            /*  if specified not exist create new */
+            if(!myDir.exists())
+            {
+                myDir.mkdir();
+                Log.v("", "inside mkdir");
+            }
+
+            /* checks the file and if it already exist delete */
+            String fname = imageName;
+            File file = new File (myDir, fname);
+            if (file.exists ())
+                file.delete ();
+
+            /* Open a connection */
+            URLConnection ucon = url.openConnection();
+            InputStream inputStream = null;
+            HttpURLConnection httpConn = (HttpURLConnection)ucon;
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+
+            if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK)
+            {
+                inputStream = httpConn.getInputStream();
+            }
+
+            FileOutputStream fos = new FileOutputStream(file);
+            int totalSize = httpConn.getContentLength();
+            int downloadedSize = 0;
+            byte[] buffer = new byte[1024];
+            int bufferLength = 0;
+            while ( (bufferLength = inputStream.read(buffer)) >0 )
+            {
+                fos.write(buffer, 0, bufferLength);
+                downloadedSize += bufferLength;
+                Log.i("Progress:","downloadedSize:"+downloadedSize+"totalSize:"+ totalSize) ;
+            }
+
+            fos.close();
+            Log.d("test", "Image Saved in sdcard..");
+        }
+        catch(IOException io)
+        {
+            io.printStackTrace();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dong/demodownloadimage/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.dong.demodownloadimage;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST = 1;
+    Button btn_save;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkPermission();
+        btn_save= (Button) findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ImageDownloadAndSave().execute("");
+
+
+                File imgFile = new  File("/sdcard/Images/test.jpg");
+
+                if(imgFile.exists()){
+
+                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                    ImageView myImage = (ImageView) findViewById(R.id.imageviewTest);
+
+                    myImage.setImageBitmap(myBitmap);
+
+                }
+            }
+        });
+    }
+    private void checkPermission() {
+        String[] listPermission = new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        boolean isOn = false;
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            //   textStatus1.setText("On");
+        } else {
+            //   textStatus1.setText("Off");
+            isOn = true;
+        }
+        if (isOn){
+            ActivityCompat.requestPermissions(this, listPermission, MY_PERMISSIONS_REQUEST);
+        }
+    }
+
+
+    public static String saveToSdCard(Bitmap bitmap, String filename) {
+
+        String stored = null;
+
+        File sdcard = Environment.getExternalStorageDirectory() ;
+
+        File folder = new File(sdcard.getAbsoluteFile(), ".your_specific_directory");//the dot makes this directory hidden to the user
+        folder.mkdir();
+        File file = new File(folder.getAbsoluteFile(), filename + ".jpg") ;
+        if (file.exists())
+            return stored ;
+
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+            stored = "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stored;
+    }
+
+}
